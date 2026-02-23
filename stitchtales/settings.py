75d42b3ebe654f30b7007b99f3d67d0f3b3776fa
sneha <sneha.naik1117@gmail.com>
2026-02-23 16:37:00 +0530

    Solving Supabase crashing issue

--- stitchtales/settings.py
+++ stitchtales/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'storages' # used for aws
+    
     'blog',
     'rest_framework',
     'rest_framework.authtoken',
